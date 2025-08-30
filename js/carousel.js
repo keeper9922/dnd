ebf7130ee1a33f53c0322b3748357a397b87e908
Andrey <elanser2005@gmail.com>
2025-08-30 03:47:17 +0300

    Починен авторазмер. Добавлены описания и картинки для остальных карточек

--- js/carousel.js
+++ js/carousel.js
@@ -5,11 +5,11 @@ next = document.querySelector(".next");
 
 let index = 0;
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(items.length);
+    // console.log(items.length);
     for (let i = 0; i < items.length; i++) {
-        // items[i].style.gridTemplateColumns = `repeat(${items[i].childNodes.length}, 1fr)`;
-        console.log(items[i].childNodes.length);
-        console.log(" ");
+        items[i].style.gridTemplateColumns = `repeat(${items[i].children.length-1}, 1fr)`;
+        // console.log("Children: " + (items[i].children.length-1));
+        // console.log("");
     }
 });
 function update() {
