c9265691d4e69be94dbdc52827f0b2c073117cd0
Andrey <elanser2005@gmail.com>
2025-09-02 13:06:05 +0300

    Добавлено больше описаний

--- js/carousel.js
+++ js/carousel.js
@@ -5,6 +5,7 @@ let next = document.querySelector(".next");
 let popup = document.querySelector(".popup");
 let popupParent = document.querySelector(".popup-fade");
 let popupClose = document.querySelector(".popup-close");
+let navButtons = document.querySelectorAll("main>header>a");
 
 let index = 0;
 function update() {
@@ -13,7 +14,36 @@ function update() {
 
 jQuery(function ($) {
     update();
-    // console.log(popup)
+    for (let i = 0; i < navButtons.length; i++) {
+        console.log($(navButtons[i]));
+        // let content = $(navButtons[i].href).find(".card-full-content")
+        // $(navButtons[i]).on("click", function ( ) {
+        //     $(content).appendTo(popup);
+        //     $(content).css("display", "block");
+        //     $(popupParent).css("display", "block");
+        // });
+        // $(document).on("keydown", function(e) {
+        //     if (e.key === "Escape") {
+        //         e.stopPropagation();
+        //         $(popupParent).css("display", "none");
+        //         $(content).appendTo(navButtons[i]);
+        //         $(content).css("display", "none");
+        //         // $('.popup-fade').fadeOut();
+        //     }
+        // });
+        // $(popupClose).on("click", function () {
+        //     $(popupParent).css("display", "none");
+        //     $(content).appendTo(navButtons[i]);
+        //     $(content).css("display", "none");
+        // })
+        // $(popupParent).on("click", function (e) {
+        //     if ($(e.target).closest(popup).length === 0) {
+        //         $(this).css("display", "none");
+        //         $(content).appendTo(navButtons[i]);
+        //         $(content).css("display", "none");
+        //     }
+        // })
+    }
     for (let i = 0; i < items.length; i++) {
         let cards = items[i].children
         items[i].style.gridTemplateColumns = `repeat(${cards.length-1}, 1fr)`;
